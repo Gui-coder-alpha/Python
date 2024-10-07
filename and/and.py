7e7d78f10e1158222e99c4748a4cca25de0a80f7
Gui-coder-github <guilhermesamt64@gmail.com>
2024-10-07 15:08:04 -0400

    and e or

---
+++ and/and.py
@@ -0,0 +1,8 @@
+#AND no python, deve ser usada como condição, neste caso o and deve ser true em todas as condições.
+#Caso uma seja falsa, o valor da condição se dá falsa
+
+#OR, diferente do and, também usada como condição, mas mesmo com um valor false
+#Ela não vai ter valor false, mas sim true, para ser false, todas as condições dever ser false.
+
+#Enquanto o and todas as condições devem ser true, or precisa que todas as condições
+#false para ser false, uma clara diferença
