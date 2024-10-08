ba0a2f5d444a5a47f13d0788b7bbafcbf70a1dbc
Gui-coder-github <guilhermesamt64@gmail.com>
2024-10-08 16:45:53 -0400

    forloop

---
+++ while/forloops.py
@@ -0,0 +1,9 @@
+#For loops, mesmo sendo baseado em repetir é diferente de while.
+#A sua característica é pricipalmente em números de repetição
+#É limitado a um número de números, sendo finito
+
+
+#1 = início  11= final   2 = número de vezes
+for x in range(1, 11, 2):
+    print(x)
+
