29e5e72886d09856a10271a530c2dc948eb3e01e
Gui-coder-github <guilhermesamt64@gmail.com>
2024-10-16 14:37:42 -0400

    membership operator

--- 1/membership.py
+++ 1/membership.py
@@ -11,3 +11,11 @@ if escreva in words:
     print(f"Yes, the fruit {escreva} here.")
 else:
     print(f"Don't have this fruit")
+
+
+#Dica caso seja dicionário
+# grades = {"garry":"a", "patrick": "b", "gui": "c"}
+#Para encontrar o valor do dicionário, que seria a b ou c
+#basta colocar da seguinte maneira
+#grades[student]
+#student = inut("your friend")
