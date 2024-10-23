2b5f9831368901f45a45d23f7cc46b1ac6675731
Gui-coder-github <guilhermesamt64@gmail.com>
2024-10-23 09:37:42 -0400

    intermediário, início
    Iniciando o intermediário em python.

---
+++ intermediário/leia.py
@@ -0,0 +1,13 @@
+#Aqui irei aprender sobre o intermediário de python.
+#Com o intuito de aprender redes neurais.
+#Agora se inicia verdadeiramente a jornada
+#do aprendizado sobre redes neurais, e
+#compreendendo mais sobre.
+#EStudar sobre:
+#Orientação a Objetos OOP
+#Estruturas de dados avançadas
+#Aprender bibliotecas como o TensorFlow, Keras, PyTorch entre outros
+#Usar APIs, aprender o mais rápido possível
+#Teste e Depuração
+#Programação assícrona
+#GUI = Desenvolvimento de interfaces gráficas
