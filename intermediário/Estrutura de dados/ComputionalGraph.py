b1c718993aa2b37fe12a0db6413ffce074601db4
Gui-coder-github <guilhermesamt64@gmail.com>
2025-01-02 17:10:49 -0400

    Grafos

---
+++ intermediário/Estrutura de dados/ComputionalGraph.py
@@ -0,0 +1,16 @@
+#Computational graphs ou grafos computacionais é nada mais que uma forma
+#de representar calculos matemáticos de forma mais atrativa, ou seja, algo mais
+#visual e bem estruturada e simplificada.
+#Um grafo computacional é formados por Nós (Nodes) e Arestas (Edges).
+#Node é nada mais do que uma representações de operações matemáticas ou variáveis
+#Edges é o fluxo de dados entre as operações, ou seja, o fluxo de informação
+#
+#Vamos pegar um exemplo para uma melhor viualização,
+#temos os seguintes Nodes de entrada:x, y e z
+#Onde queremos a seguinte expressão w = (x+z) * y
+#Para representar esta expressão como um grafo computacional, teremos:
+# a = x + z
+# w = a * y
+#basicamente criamos um gráfico onde temos um gráfico de entrada e um gráfico de saída
+#
+#
