0d4eccab566625398d30f0d56669481aaa89739c
Gui-coder-github <guilhermesamt64@gmail.com>
2025-05-03 15:37:04 -0400

    Broadcasting Update

--- intermediário/Numpy/Broadncasting.py
+++ intermediário/Numpy/Broadncasting.py
@@ -24,6 +24,30 @@ desvio1 = np.std(dados1, axis=0) #desvio realizado
 
 divisao1 = (dados1 - media1) / desvio1
 print(divisao1)
+print("//////////////////////////////////////////////////////////")
 
+#Selecção condicional de colunas, booleanas
+dados2 = np.array([[101, 250.50, 5],
+                   [102, 150.00, 10],
+                   [103, 300.75, 3],
+                   [104, 120.00, 8],
+                   [105, 400.00, 6]])
 
+dados_da_coluna = dados2[0: ,1] #vírgula obrigatória, se orientar igual ao plano cartesiano [linha, coluna]
+
+valor_alto_200 = dados_da_coluna > 200
+
+valor_para_dados2 = dados2[valor_alto_200] #Pegou somente valores altos com base na variável valor_alto_200, e aplicando para o resto da matriz como um conjunto inteiro
+print(valor_para_dados2)
+print("//////////////////////////////////////////////////////////")
+
+#Reordenação de linhas(Indexação Fancy)
+matriz_original = np.array([[1, 2, 3],
+                            [4, 5, 6],
+                            [7, 8, 9],
+                            [10, 11, 12]])
+
+matriz_reordenada = matriz_original[[3, 0, 1, 2]] #Reordenando as linhas da matriz original
+print(matriz_reordenada)
+print("//////////////////////////////////////////////////////////")
 
